5480ee96aa30cd3d7afdc530149cd9a4f0838bfa
AndrejOvecka <andrej.ovecka@gmail.com>
2019-01-19 14:32:16 +0100

    1111

--- app/src/main/java/com/example/andre/cocktailapp/MainActivity.java
+++ app/src/main/java/com/example/andre/cocktailapp/MainActivity.java
@@ -23,9 +23,8 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+private Button Button;
 
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,46 +33,28 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Button button = findViewById(R.id.Button);
+        Button = (Button) findViewById(R.id.Button);
         final TextView textView = findViewById(R.id.textView2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncTask asyncTask = new AsyncTask() {
-                    @Override
-                    protected Object doInBackground(Object[] objects) {
-                        OkHttpClient client = new OkHttpClient();
-                        Request request = new Request.Builder()
-                                .url("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-                                .build();
-                        Response response = null;
-
-                        try{
-                            response = client.newCall(request).execute();
-                            return response.body().string();
-                        }
-                        catch (IOException e)
-                        {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    protected void onPostExecute(Object o) {
-                        textView.setText(o.toString());
-                    }
-                }.execute();
+                openRandomDrink();
             }
+
+
+
         });
+    }
+
+    public void openRandomDrink(){
+        Intent intent  = new Intent(this, RandomDrink.class);
+        startActivity(intent);
     }
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-       // String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, "ahoj");
+
         startActivity(intent);
 
     }
